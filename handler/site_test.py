5cd6d466e855f90433ce3a567bc26cf55370673b
Jason Fan <sevengram1991@gmail.com>
2015-03-05 17:22:08 +0800

    update error

--- handler/site_test.py
+++ handler/site_test.py
@@ -13,11 +13,11 @@ import wxclient
 class TestHandler(SiteBaseHandler):
     @tornado.gen.coroutine
     def get(self, siteid):
-        # browser = MockBrowser()
-        # yield browser.login('wxfc87c2547449c2c6')
+        browser = MockBrowser()
+        yield browser.login('wxfc87c2547449c2c6')
         # yield browser.send_single_message('wxfc87c2547449c2c6', '1899417504', u'你好')
-        result = yield wxclient.get_user_info('wxfc87c2547449c2c6', 'owPaGjv_SB5DnzAQN4_knohnvC_I')
-        # result = yield browser.find_user('wxfc87c2547449c2c6', 1425542418, u'云南省昆明市东川区', 'text')
+        # result = yield wxclient.get_user_info('wxfc87c2547449c2c6', 'owPaGjv_SB5DnzAQN4_knohnvC_I')
+        result = yield browser.find_user('wxfc87c2547449c2c6', 1425542418, u'云南省昆明市东川区', 'text')
         print result
         sys.stdout.flush()
         self.send_response(data=result.get('data', ''), err_code=result.get('err_code', 0))
